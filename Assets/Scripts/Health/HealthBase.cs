77c0963f00ab77816c2f1b545261c1c0af33d362
witchis <mctp2002@gmail.com>
2025-11-06 01:22:39 -0500

    Add Health & Enemy

---
+++ Assets/Scripts/Health/HealthBase.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    public int startLife = 10;
+
+    public bool destroyOnKill = true;
+    public float delayToKill = 1f;
+
+    private int _currentLife;
+    private bool _isDead = false;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _isDead = false;
+        _currentLife = startLife;
+    }
+
+    public void Damage(int damage)
+    {
+        if (_isDead) return;
+
+        _currentLife -= damage;
+
+        if (_currentLife <= 0)
+        {
+            Kill();
+        }
+    }
+
+    private void Kill()
+    {
+        _isDead = true;
+
+        if (destroyOnKill)
+        {
+            Destroy(gameObject, delayToKill);
+        }
+    }
+}
